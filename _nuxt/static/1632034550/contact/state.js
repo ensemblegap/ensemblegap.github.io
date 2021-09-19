0ae093511e23e73305622484de99f8e8bdfcb934
root <y.lifelog@gmail.com>
2021-09-19 06:55:53 +0000

    5f0e06b

---
+++ _nuxt/static/1632034550/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632034550",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{gtm:{id:"GTM-WQR99HV"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"42c7a847"}}}}(null));
---
+++ _nuxt/static/1632034550/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{rendering:"sesrver"}],fetch:{},mutations:void 0});
